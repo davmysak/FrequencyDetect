280e9aca8108599a23bf4591a9b221936846d8c2
David Mysak <davmysak@gmail.com>
2023-06-01 18:58:34 +0200

    Update app.js
    Minor fix

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ oscillator.type = "sine"; // Set the waveform type
 oscillator.frequency.setValueAtTime(220, audioContext.currentTime); // Set the frequency to 220 Hz
 // Create a gain node to control the volume
 const gainNode = audioContext.createGain({
-	gain: 0.5
-}); // Set the initial gain to 0.5
+	gain: 0.5 // Set the initial gain to 0.5
+}); 
 // Connect the oscillator to the gain node, and the gain node to the audio context
 oscillator.connect(gainNode).connect(audioContext.destination);
 // Start the oscillator
